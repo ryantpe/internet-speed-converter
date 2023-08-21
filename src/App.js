9ebbfe25290995c1d831c4ae56dee1eed6a75ed0
ryantpe <ipmoid@gmail.com>
2023-08-21 16:06:01 +0800

    v1.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [inputValue, setInputValue] = useState(0);
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    setInputValue(value);
+  };
 
   return (
     <div className="container">
@@ -17,14 +24,14 @@ function App() {
             <div className="converter">
               <div className="flex-1">
                 <div className="convert-title">Set</div>
-                <input type="number" className="input-number" min="0" onChange={()=>console.log('onchange')} />
+                <input type="number" className="input-number" min="0" onChange={handleInputChange} />
               </div>
               <span className="angle-icon fa-2x" style={{marginTop: "30px"}}>
                 <i className='fas fa-angle-right'></i>
               </span>
               <div className='text-right flex-1'>
                 <div className='convert-title'>Show</div>
-                <input type='text' className='input-number text-right' value={"125"} disable />
+                <input type='text' className='input-number text-right' value={inputValue} disable />
               </div>
             </div>
         </div>
